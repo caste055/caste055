57c6b3770af8e2661a7cb70d7b85b12a40caed1c
caste055 <42884488+caste055@users.noreply.github.com>
2020-04-01 17:55:37 -0700

    Add files via upload

---
+++ Qualcommserver.py
@@ -0,0 +1,40 @@
+from socket import*
+
+qualcommRRTable = {
+    "number1":
+    {
+        "TransactionID": "1",
+        "Name": "www.qualcomm.com",
+        "Type": "A",
+        "Value": "104.86.224.205",
+        "TTL": "",
+        "Static" : "1"
+        },
+    "number2":
+    {
+        "TransactionID": "2",
+        "Name": "qtiack12.qti.qualcomm.com",
+        "Type": "A",
+        "Value": "129.46.100.21",
+        "TTL": "",
+        "Static" : "1"
+        }
+    }
+
+
+serverPort = 21000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print ('The server is ready to receive')
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    DNSQuery, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode()
+    DNSModified = DNSQuery.decode()
+    for item in qualcommRRTable.values():
+        if(item['Name'] == modifiedMessage and item['Type'] == DNSModified):
+            print("Name Found! It's value is " + item['Value'])
+            modifiedMessage = item['Value']
+            print("It is now modified " + modifiedMessage)
+
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
